5055ef441788822949b0a81d6195a1866b4978d1
ShooterCrow <cydollarsign1@gmail.com>
2025-11-03 05:03:32 +0100

    Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 exports.allowedOrigins = [
     ...(process.env.NODE_ENV !== "production" ? ['http://localhost:5174', 'http://localhost:5173'] : []),
-    'https://workearn.online/',
-    'https://workearn.online',
-    'workearn.online/',
-    'workearn.online',
-    "https://www.workearn.online",
+    'https://modloot.xyz/',
+    'https://modloot.xyz',
+    'modloot.xyz/',
+    'modloot.xyz',
+    "https://www.modloot.xyz",
     "https://goldengoose-kappa.vercel.app"
 ];
