ef4318d574465b014d778dc8ef3bc2399ecfa964
ShooterCrow <cydollarsign1@gmail.com>
2025-11-03 05:19:57 +0100

    Update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ exports.allowedOrigins = [
     ...(process.env.NODE_ENV !== "production" ? ['http://localhost:5174', 'http://localhost:5173'] : []),
     'https://modloot.xyz/',
     'https://modloot.xyz',
-    'https://www.modloot.xyz',
+    'https://www.modloot.xyz', 
     'modloot.xyz/',
     'modloot.xyz', 
     "https://www.modloot.xyz",
